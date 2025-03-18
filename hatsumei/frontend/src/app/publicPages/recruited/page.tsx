6164a3a6d4644005c2424e29ce7f4a06fa2998ed
青木 真莉菜 <m.aoki.258@gmail.com>
2025-03-18 18:14:04 +0900

    delete : コメント削除

--- hatsumei/frontend/src/app/publicPages/recruited/page.tsx
+++ hatsumei/frontend/src/app/publicPages/recruited/page.tsx
@@ -24,27 +24,24 @@ const RecruitedPage = () => {
       </div>
 
       <div className="flex justify-center items-center mt-10 mb-20 bg-white relative">
-        {/* 親要素に左右の余白を追加して、最大幅を設定 */}
         <div className="px-3 max-w-screen-xl w-full">
           <div className="flex overflow-x-auto py-4 px-2 gap-4">
             {" "}
-            {/* gapを調整してカード間の隙間を狭める */}
             {ideaPosts.map((post, index) => {
               const user = users.find((u) => u.id === post.user_id);
-              const image = images[index % images.length]; // 画像をサイクル
+              const image = images[index % images.length];
 
               return (
                 <div
                   key={post.id}
-                  className="flex-none w-[calc(25%-20px)] h-auto p-4 bg-gray-100 shadow-lg rounded-lg" // 横並びで4つにするための幅設定
+                  className="flex-none w-[calc(25%-20px)] h-auto p-4 bg-gray-100 shadow-lg rounded-lg"
                 >
                   <div className="relative w-full">
-                    {/* 画像をボタンにする */}
                     <Link href={`/post/${post.id}`}>
                       <img
-                        src={image} // 画像パス
+                        src={image}
                         alt="投稿画像"
-                        className="w-full h-[200px] object-cover rounded-t-lg cursor-pointer" // 横長に画像の高さを調整
+                        className="w-full h-[200px] object-cover rounded-t-lg cursor-pointer"
                       />
                     </Link>
                     <div className="p-4">
